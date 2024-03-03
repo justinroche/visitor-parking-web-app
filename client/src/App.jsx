e00f921494b48c98b61981898a707c77a2c8c10f
justinroche <justinroche03@gmail.com>
2024-03-03 12:05:51 -0600

    Initialize front-end (React and Axios)

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Visitor Parking</h1>
+    </>
+  );
+}
+
+export default App;
