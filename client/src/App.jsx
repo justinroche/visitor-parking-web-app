5439ee8d16727ee9df9e7408ea552174a2d2b265
justinroche <justinroche03@gmail.com>
2024-03-04 18:54:17 -0600

    Add Bootstrap, Create demo modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,39 @@
 import { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import CustomModal from './CustomModal';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [showCustomModal, setShowCustomModal] = useState(false);
+
+  const handleCloseCustomModal = () => setShowCustomModal(false);
+  const handleShowCustomModal = () => setShowCustomModal(true);
+
+  function AppHeader() {
+    return (
+      <div className="App-header">
+        <h1>Visitor Parking</h1>
+      </div>
+    );
+  }
+
+  function AppMain() {
+    return (
+      <div className="App-main">
+        <Button variant="primary" onClick={handleShowCustomModal}>
+          Show demo modal
+        </Button>
+        <CustomModal show={showCustomModal} handleClose={handleCloseCustomModal} />
+      </div>
+    );
+  }
 
   return (
     <>
-      <h1>Visitor Parking</h1>
+      <AppHeader />
+      <AppMain />
     </>
   );
 }
---
+++ client/src/CustomModal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+function CustomModal({ show, handleClose }) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Modal body</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default CustomModal;
