0aaed7df12c12571a292ec51d74eba4d255f5a44
justinroche <justinroche03@gmail.com>
2024-03-18 00:08:34 -0500

    Remove demo modal from App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,12 @@ import axios from 'axios';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import PurchasePassModal from './PurchasePassModal';
-import DemoModal from './DemoModal';
 
 function App() {
   /* State */
-  const [showDemoModal, setShowDemoModal] = useState(false);
   const [showPurchasePassModal, setShowPurchasePassModal] = useState(false); // State for purchase a pass modal
 
   /* Handlers */
-  const handleCloseDemoModal = () => setShowDemoModal(false);
-  const handleShowDemoModal = () => setShowDemoModal(true);
 
   const handleClosePurchasePassModal = () => setShowPurchasePassModal(false);
   const handleShowPurchasePassModal = () => setShowPurchasePassModal(true);
@@ -30,15 +26,9 @@ function App() {
   function AppMain() {
     return (
       <div className="App-main">
-        <Button variant="primary" onClick={handleShowDemoModal}>
-          Show demo modal
-        </Button>
-        <br></br>
-        <br></br>
         <Button variant="primary" onClick={handleShowPurchasePassModal}>
           Purchase a pass
         </Button>
-        <DemoModal show={showDemoModal} handleClose={handleCloseDemoModal} />
         <PurchasePassModal
           show={showPurchasePassModal}
           handleClose={handleClosePurchasePassModal}
