3265abdc051137926b5c302493b5c7093434a4c4
Cynthia <158005567+PhillipsUWW@users.noreply.github.com>
2024-04-05 20:53:29 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import React from 'react';
 import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import DemoModal from './DemoModal';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import PurchasePassModal from './PurchasePassModal';
+import leftImage from './CampusMap.jpg';
 
 function App() {
   /* State */
-  const [showDemoModal, setShowDemoModal] = useState(false);
+    const [showPurchasePassModal, setShowPurchasePassModal] = useState(false); // State for purchase a pass modal
 
   /* Handlers */
-  const handleCloseDemoModal = () => setShowDemoModal(false);
-  const handleShowDemoModal = () => setShowDemoModal(true);
+  const handleClosePurchasePassModal = () => setShowPurchasePassModal(false);
+  const handleShowPurchasePassModal = () => setShowPurchasePassModal(true);
 
   /* Components */
   function AppHeader() {
@@ -22,15 +23,43 @@ function App() {
     );
   }
 
+  function MyComponent() {
+    return (
+      <div className="container">
+        <img src={leftImage} alt="Left Image" className="left-image" />
+        
+      </div>
+    );
+  }
+  
+  function TextBox () {
+    return (
+      <div className = "container">
+        <div className = "text-container">
+          <p>Parking on Campus</p>
+           <p>A parking pass is required at all times except for 
+           5 p.m. Fridays to 11:00 p.m. Sundays</p>
+          <p>and Univeristy recognized holidays</p>
+        </div>
+
+      </div>
+    )
+  }
+
   function AppMain() {
     return (
-      <div className="App-main">
-        <Button variant="primary" onClick={handleShowDemoModal}>
-          Show demo modal
-        </Button>
-        <DemoModal
-          show={showDemoModal}
-          handleClose={handleCloseDemoModal}
+      <div className="App-main">  
+        <div className = "button1">
+         <Button variant="primary" style={{backgroundColor: '#4b0082', color: 'white', border: 'none'}} onClick={handleShowPurchasePassModal}>
+         Purchase a pass
+          </Button>
+        </div>
+          <PurchasePassModal
+          show={showPurchasePassModal}
+          handleClose={handleClosePurchasePassModal}
+          isLoggedIn={true}
+          
+          
         />
       </div>
     );
@@ -41,8 +70,11 @@ function App() {
     <>
       <AppHeader />
       <AppMain />
+      <MyComponent />
+      <TextBox />
     </>
   );
 }
 
 export default App;
+
