3c18649a81aeb7b120526ece217e5e00453d31d2
Cynthia <158005567+PhillipsUWW@users.noreply.github.com>
2024-04-09 17:53:25 -0500

    Update App.jsx
    testing adding multiple features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,29 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import PurchasePassModal from './PurchasePassModal';
 import ParkingInformation from './ParkingInformation';
+import
 
 
 function App() {
   /* State */
  
   const [showParkingInformation, setShowParkingInformation] = useState(false);
+  const [showPurchasePassModal, setShowPurchasePassModal] = useState(false); // State for purchase a pass modal
 
 
   /* Handlers */
   const handleCloseParkingInformation = () => setShowParkingInformation(false);
   const handleShowParkingInformation = () => setShowParkingInformation(true);
+  const handleClosePurchasePassModal = () => setShowPurchasePassModal(false);
+  const handleShowPurchasePassModal = () => setShowPurchasePassModal(true);
 
   /* Components */
   function AppHeader() {
     return (
       <div className="App-header">
-        <h1>Parking Information</h1>
+        <h1>Visitor Parking</h1>
       </div>
     );
   }
@@ -40,12 +45,31 @@ function App() {
       </div>
     );
   }
+  function AppPurchasePass() {
+    return (
+      <div className="App-main">
+       div className = "button2">
+         <Button variant="primary" style={{backgroundColor: '#4b0082', color: 'white', border: 'none'}} onClick={handleShowPurchasePassModal}>
+         Purchase a pass
+          </Button>
+        </div>
+          <PurchasePassModal
+          show={showPurchasePassModal}
+          handleClose={handleClosePurchasePassModal}
+          isLoggedIn={true}
+         />
+      </div>
+    );
+  }
 
+          
+ 
   /* Render */
   return (
     <>
       <AppHeader />
       <AppMain />
+      <AppPurchasePass />
     </>
   );
 }
