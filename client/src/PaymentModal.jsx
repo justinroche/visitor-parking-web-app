1c42e9ae3b47dde6045c8ae034c9df7e9ca9c4f1
patyj6 <WhitneyPJ14@uww.edu>
2024-04-16 10:16:53 -0500

    modified expiration date verification

--- client/src/PaymentModal.jsx
+++ client/src/PaymentModal.jsx
@@ -91,7 +91,7 @@ function PaymentModal({ show, handleClose, purchasePassData }) {
     }
 
     /* Verify MM/YY expiration date */
-    const expirationDateRegex = /^\d{2}\/\d{2}$/;
+    const expirationDateRegex = /^(0[1-9]|1[0-2])\/([0-9]{2})$/;
     if (!expirationDateRegex.test(paymentData.expirationDate)) {
       alert('Please enter a valid mm/yy expiration date.');
       return;
