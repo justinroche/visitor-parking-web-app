64ee77cb6324b38f9f73ce22544314bde680c186
justinroche <justinroche03@gmail.com>
2024-04-18 15:31:45 -0500

    Updated connection pool

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const pool = mysql.createPool({
-  host: 'washington',
+  host: 'washington.uww.edu',
   user: 'rochejd20',
   password: 'jr1649',
   database: 'uww-visitor-parking',
