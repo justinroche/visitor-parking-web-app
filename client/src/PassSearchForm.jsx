6c511273e6540f2d32b372eaa65973b53499d55c
justinroche <justinroche03@gmail.com>
2024-04-18 22:23:28 -0500

    Complete pass search form

--- client/src/PassSearchForm.jsx
+++ client/src/PassSearchForm.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import { Form, FormControl, Button, Spinner } from 'react-bootstrap';
+import './PassSearchForm.css';
 
 function PassSearchForm() {
   const [plateSearch, setPlateSearch] = useState('');
   const [endTime, setEndTime] = useState('');
   const [passExists, setPassExists] = useState(false);
   const [searched, setSearched] = useState(false);
+  const [loading, setLoading] = useState(false); // Add loading state
 
-  const handleSearchChange = (event) => {
-    setPlateSearch(event.target.value);
-  };
+  // Max length for license plate is 7 characters.
+  useEffect(() => {
+    if (plateSearch.length > 7) {
+      setPlateSearch(plateSearch.substring(0, 7));
+    }
+  }, [plateSearch]);
+
+  function handleSearchChange(event) {
+    setPlateSearch(event.target.value.toUpperCase());
+  }
 
   const handleSearchSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true); // Set loading to true when search starts
 
     await axios
       .post('http://localhost:8080/pass-search', {
@@ -21,11 +31,10 @@ function PassSearchForm() {
       })
       .then((response) => {
         console.log(response);
+        setPlateSearch(''); // Clear the search input after search
         if (response.data.message === 'Pass information not found') {
-          console.log('no pass found');
           setPassExists(false);
         } else {
-          console.log('pass found');
           setPassExists(true);
           setEndTime(new Date(response.data.passInfo.endTime).toLocaleString());
         }
@@ -33,26 +42,37 @@ function PassSearchForm() {
       })
       .catch((error) => {
         console.error('Error:', error);
+      })
+      .finally(() => {
+        setLoading(false); // Set loading to false when search completes
       });
   };
 
   return (
     <>
-      <Form inline="true" onSubmit={handleSearchSubmit}>
+      <Form id="passSearchForm" onSubmit={handleSearchSubmit}>
+        <h6>Search for an existing pass</h6>
         <FormControl
+          id="passSearchInput"
           type="text"
-          placeholder="Search for an existing pass"
+          placeholder="Enter a license plate"
           value={plateSearch}
           onChange={handleSearchChange}
         />
-        <br />
-        <Button variant="outline-primary" type="submit">
+        <Button id="passSearchButton" variant="outline-primary" type="submit">
           Search
         </Button>
       </Form>
-      <br />
-      {searched && passExists && <p>✅ Pass ends at {endTime}</p>}
-      {searched && !passExists && <p>❌ No pass found</p>}
+      <div id="passSearchResults">
+        {loading && <Spinner animation="border" variant="primary" />}
+        {searched && passExists && !loading && (
+          <>
+            <p>✅ Pass found</p>
+            <p>Pass ends at {endTime}</p>
+          </>
+        )}
+        {searched && !passExists && !loading && <p>❌ No pass found</p>}
+      </div>
     </>
   );
 }
