7f7837c690e6db8bd0e4298f488454d135a1c43c
Cynthia <158005567+PhillipsUWW@users.noreply.github.com>
2024-04-19 22:50:01 -0500

    Create DateTime.jsx
    add to branch

---
+++ client/src/DateTime.jsx
@@ -0,0 +1,32 @@
+import  React, { useState , useEffect } from 'react'
+
+export const DateTime = () => {
+
+    var [date,setDate] = useState(new Date());
+    
+    useEffect(() => {
+        var timer = setInterval(()=>setDate(new Date()), 1000 )
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    
+    });
+
+    const formatDate = (date) => {
+        const options = { 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric' 
+        };
+        return date.toLocaleDateString(undefined, options);
+    };
+
+    return(
+        <div>
+            <p> Today is {formatDate(date)}</p>
+            <p>{date.toLocaleTimeString()}</p>
+        </div>
+    )
+}
+
+export default DateTime;
