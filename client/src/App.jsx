aa919b4d0504b0e508426c675104f34023e74f8b
justinroche <justinroche03@gmail.com>
2024-04-22 00:05:22 -0500

    Work in progress update to log in and account system

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Button from 'react-bootstrap/Button';
 import DemoModal from './DemoModal';
 import PaymentModal from './PaymentModal';
 import ParkingInfoModal from './ParkingInfoModal';
-import uww_logo from "./UWWhitewater_logo.png";
+import uww_logo from './UWWhitewater_logo.png';
 import LoginModal from './LoginModal';
 import AccountSettingsModal from './AccountSettingsModal';
 import AddTimeModal from './AddTimeModal';
@@ -20,13 +20,14 @@ function App() {
   const [purchasePassData, setPurchasePassData] = useState(null); // State for purchase pass data (license plate, credit card, etc.)
   const [showParkingInfoModal, setShowParkingInfoModal] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
-  const [showAccountSettingsModal, setShowAccountSettingsModal] = useState(false);
+  const [showAccountSettingsModal, setShowAccountSettingsModal] =
+    useState(false);
   const [showAddTimeModal, setShowAddTimeModal] = useState(false);
-  
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState('');
 
   /* Handlers */
-  const handleClosePurchasePassModal = () => setShowPurchasePassModal(false); 
+  const handleClosePurchasePassModal = () => setShowPurchasePassModal(false);
   const handleShowPurchasePassModal = () => {
     setPurchasePassData(null);
     setShowPurchasePassModal(true);
@@ -34,36 +35,64 @@ function App() {
   const handleClosePaymentModal = () => setShowPaymentModal(false);
   const handleShowPaymentModal = () => setShowPaymentModal(true);
   //const handlePaymentModalOpen = () => setShowPaymentModal(true);
-  const handleCloseParkingInfoModal = () => setShowParkingInfoModal(false); 
+  const handleCloseParkingInfoModal = () => setShowParkingInfoModal(false);
   const handleShowParkingInfoModal = () => setShowParkingInfoModal(true);
   const handleCloseLoginModal = () => setShowLoginModal(false);
   const handleShowLoginModal = () => setShowLoginModal(true);
-  const handleCloseAccountSettingsModal = () => setShowAccountSettingsModal(false);
-  const handleShowAccountSettingsModal = () => setShowAccountSettingsModal(true);
+  const handleCloseAccountSettingsModal = () =>
+    setShowAccountSettingsModal(false);
+  const handleShowAccountSettingsModal = () =>
+    setShowAccountSettingsModal(true);
   const handleCloseAddTimeModal = () => setShowAddTimeModal(false);
   const handleShowAddTimeModal = () => setShowAddTimeModal(true);
+  const handleLogin = (email) => {
+    setIsLoggedIn(true);
+    setUserEmail(email);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUserEmail('');
+  };
 
   /* Components */
   function AppHeader() {
     return (
       <div className="App-header">
-        <div className='top-bar'>
+        <div className="top-bar">
           <img src={uww_logo} alt="uww image" className="whitewater-logo" />
           <h1>Visitor Parking</h1>
-          
-          <div className="button-container">
-            <Button className='account-settings' variant='secondary' onClick={handleShowAccountSettingsModal}>
-              Account Settings
-            </Button> 
-            <Button className='login-button' variant='secondary' onClick={handleShowLoginModal}>
-              Login
-            </Button>
-            <Button className='logout-button' variant='secondary'>
-              Log Out
-            </Button>
-        </div>
 
-        </div>  
+          <div className="button-container">
+            {isLoggedIn && (
+              <Button
+                className="account-settings"
+                variant="secondary"
+                onClick={handleShowAccountSettingsModal}
+              >
+                Account Settings
+              </Button>
+            )}
+            {!isLoggedIn && (
+              <Button
+                className="login-button"
+                variant="secondary"
+                onClick={handleShowLoginModal}
+              >
+                Login
+              </Button>
+            )}
+            {isLoggedIn && (
+              <Button
+                className="logout-button"
+                variant="secondary"
+                onClick={handleLogout}
+              >
+                Log Out
+              </Button>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
@@ -79,95 +108,133 @@ function App() {
   function AppMain() {
     return (
       <div className="App-main">
-      <div className="row">
-        <div className="col">
-          <Button id ='info-button' className="modal-button" variant="primary" onClick={handleShowParkingInfoModal}>
-            Parking Pass Information
-          </Button>
-          <br />
-          <br />
-          <Button id = 'buy-pass-button' className="modal-button" variant="secondary" onClick={handleShowPurchasePassModal}>
-            Buy A Pass
-          </Button>
-          <br/>
-          <br/>
-
+        <div className="row">
+          <div className="col">
+            <Button
+              id="info-button"
+              className="modal-button"
+              variant="primary"
+              onClick={handleShowParkingInfoModal}
+            >
+              Parking Pass Information
+            </Button>
+            <br />
+            <br />
+            <Button
+              id="buy-pass-button"
+              className="modal-button"
+              variant="secondary"
+              onClick={handleShowPurchasePassModal}
+            >
+              Buy A Pass
+            </Button>
+            <br />
+            <br />
           </div>
 
+          <div className="col">
+            {!isLoggedIn && (
+              <h5 className="log-in-message">
+                Please sign in or create an account to view your passes.
+              </h5>
+            )}
+            {isLoggedIn && (
+              <table className="current-passes">
+                <thead>
+                  <tr>
+                    <th>
+                      <h5
+                        style={{ marginBottom: '30px' }}
+                        className="user-passes"
+                      >
+                        User's Current Passes
+                      </h5>
+                    </th>
+                  </tr>
+                  <tr className="header-row">
+                    <th>Plate Identification</th>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Year</th>
+                    <th>Time Remaining</th>
+                    <th>Add Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>862-PCG</td>
+                    <td>Jeep</td>
+                    <td>Gladiator</td>
+                    <td>2024</td>
+                    <td>31:02</td>
+                    <td>
+                      <Button
+                        className="add-time-button"
+                        variant="secondary"
+                        onClick={handleShowAddTimeModal}
+                      >
+                        Add Time
+                      </Button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            )}
+          </div>
 
-        <div className="col">
-          <table className="current-passes">
-            <thead>
-            <h5 style={{marginBottom: '30px'}} className='user-passes'>User's Current Passes</h5>
-              <tr className='header-row'>
-                <th>Plate Identification</th>
-                <th>Make</th>
-                <th>Model</th>
-                <th>Year</th>
-                <th>Time Remaining</th>
-                <th>Add Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>862-PCG</td>
-                <td>Jeep</td>
-                <td>Gladiator</td>
-                <td>2024</td>
-                <td>31:02</td>
-                <Button className="add-time-button" variant="secondary" onClick={handleShowAddTimeModal}>
-                  Add Time
-                </Button>
-              </tr>
-            </tbody>
-          </table>
+          <div className="col">
+            <div className="availability">
+              <h5>{isLoggedIn && `Welcome, ${userEmail}`}</h5>
+            </div>
+            <div className="availability">
+              <DateTime />
+            </div>
+            <div className="availability">
+              <h5>Current Availability</h5>
+              <p>27 spots remaining</p>
+            </div>
+          </div>
         </div>
 
+        <br />
+        <br />
 
-        <div className='col'>
-          <div className='availability'>
-            <h5>Welcome test_user!</h5>
-          </div>
-          <div className='availability'>
-            <DateTime></DateTime>
-          </div>
-          <div className='availability'>
-            <h5>Current Availability</h5>
-          <p>27 spots remaining</p>
-          </div>
-         
-        </div>
+        <LoginModal
+          show={showLoginModal}
+          handleClose={handleCloseLoginModal}
+          handleLogin={handleLogin}
+        />
+        <AccountSettingsModal
+          show={showAccountSettingsModal}
+          handleClose={handleCloseAccountSettingsModal}
+          isLoggedIn={isLoggedIn}
+        />
+        <ParkingInfoModal
+          show={showParkingInfoModal}
+          handleClose={handleCloseParkingInfoModal}
+        />
+        <PurchasePassModal
+          show={showPurchasePassModal}
+          handleClose={handleClosePurchasePassModal}
+          isLoggedIn={isLoggedIn}
+          handleShowPaymentModal={handleShowPaymentModal}
+          setPurchasePassData={setPurchasePassData}
+        />
+        <PaymentModal
+          show={showPaymentModal}
+          handleClose={handleClosePaymentModal}
+          purchasePassData={purchasePassData}
+        />
+        <AddTimeModal
+          show={showAddTimeModal}
+          handleClose={handleCloseAddTimeModal}
+          isLoggedIn={isLoggedIn}
+          handleShowPaymentModal={handleShowPaymentModal}
+          setPurchasePassData={setPurchasePassData}
+        />
       </div>
-
-
-      <br/>
-      <br/>
-
-      <LoginModal show={showLoginModal} handleClose={handleCloseLoginModal} isLoggedIn={false} />
-      <AccountSettingsModal show={showAccountSettingsModal} handleClose={handleCloseAccountSettingsModal} isLoggedIn={true} />
-      <ParkingInfoModal show={showParkingInfoModal} handleClose={handleCloseParkingInfoModal} />
-      <PurchasePassModal 
-        show={showPurchasePassModal} 
-        handleClose={handleClosePurchasePassModal} 
-        isLoggedIn={true}
-        handleShowPaymentModal={handleShowPaymentModal}
-        setPurchasePassData={setPurchasePassData}
-      />
-      <PaymentModal 
-        show={showPaymentModal} 
-        handleClose={handleClosePaymentModal} 
-        purchasePassData={purchasePassData}
-      />
-      <AddTimeModal 
-        show={showAddTimeModal} 
-        handleClose={handleCloseAddTimeModal}
-        isLoggedIn={true}
-        handleShowPaymentModal={handleShowPaymentModal}
-        setPurchasePassData={setPurchasePassData} />
-    </div>
     );
   }
-
 
   /* Render */
   return (
--- client/src/LoginModal.jsx
+++ client/src/LoginModal.jsx
@@ -5,97 +5,130 @@ import axios from 'axios';
 import './LoginModal.css';
 import CreateAccountModal from './CreateAccountModal';
 
-function LoginModal({ show, handleClose, isLoggedIn }) {
-    /* State */
-    const [createAccountModalVisible, setCreateAccountModalVisible] = useState(false);
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
-    const [passwordVisibility, setPasswordVisibility] = useState(false);
+function LoginModal({ show, handleClose, handleLogin }) {
+  /* State */
+  const [createAccountModalVisible, setCreateAccountModalVisible] =
+    useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
 
-    /* Handlers */
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  /* Handlers */
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const togglePasswordVisibility = () => {
-        setPasswordVisibility(!passwordVisibility);
-    };
+  const togglePasswordVisibility = () => {
+    setPasswordVisibility(!passwordVisibility);
+  };
 
-    const handleOpenCreateAccountModal = () => {
-        setCreateAccountModalVisible(true);
-    };
+  const handleOpenCreateAccountModal = () => {
+    setCreateAccountModalVisible(true);
+  };
 
-    const handleCloseCreateAccountModal = () => {
-        setCreateAccountModalVisible(false);
-    };
+  const handleCloseCreateAccountModal = () => {
+    setCreateAccountModalVisible(false);
+  };
 
-    const handleLogin = () => {
-        // Perform login logic here
-        console.log('Logging in with:', formData);
-        handleClose();
-    };
+  const handleLoginAttempt = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/login-user',
+        formData
+      );
+      if (response.status === 200) {
+        const userData = response.data;
+        handleClose(); // Close the modal on successful login
+        handleLogin(formData.email); // Update the login state
+      }
+    } catch (error) {
+      console.error('Login error:', error);
 
-    return (
-        <Modal show={show} onHide={handleClose} backdrop="static">
-            <Modal.Header closeButton>
-                <Modal.Title>Login</Modal.Title>
-            </Modal.Header>
+      if (error.response) {
+        const status = error.response.status;
+        if (status === 401) {
+          alert('Invalid email or password.');
+        } else {
+          alert(`Login failed with error code: ${status}`);
+        }
+      } else {
+        alert('An error occurred during login. Please try again later.');
+      }
+    }
+  };
 
-            <Modal.Body>
-                {/* Email input */}
-                <form>
-                    <input
-                        type='email'
-                        id='emailInput'
-                        name='email'
-                        placeholder='Email Address'
-                        value={formData.email}
-                        onChange={handleInputChange}
-                    />
-                </form>
-                <br />
+  return (
+    <Modal show={show} onHide={handleClose} backdrop="static">
+      <Modal.Header closeButton>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
 
-                {/* Password input */}
-                <div>
-                    <input
-                        type={passwordVisibility ? 'text' : 'password'}
-                        id='passwordInput'
-                        name='password'
-                        placeholder='Password'
-                        value={formData.password}
-                        onChange={handleInputChange}
-                    />
-                    &nbsp;&nbsp;
-                    <Button className='save-button' id='password-button' onClick={togglePasswordVisibility}>
-                        {passwordVisibility ? 'Hide Password' : 'Show Password'}
-                    </Button>
-                </div>
-                <br />
+      <Modal.Body>
+        {/* Email input */}
+        <form>
+          <input
+            type="email"
+            id="emailInput"
+            name="email"
+            placeholder="Email Address"
+            value={formData.email}
+            onChange={handleInputChange}
+            tabIndex={1}
+            required
+          />
+        </form>
+        <br />
 
-                {/* CreateAccountModal */}
-                <p id='footer-text'>Don't have an account?&nbsp;&nbsp;<button type='button' onClick={handleOpenCreateAccountModal}>Register</button></p>
-                <CreateAccountModal
-                    show={createAccountModalVisible}
-                    handleClose={handleCloseCreateAccountModal}
-                />
-                <br /><br />
-            </Modal.Body>
+        {/* Password input */}
+        <div>
+          <input
+            type={passwordVisibility ? 'text' : 'password'}
+            id="passwordInput"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
+            tabIndex={2}
+            required
+          />
+          &nbsp;&nbsp;
+          <Button id="password-button" onClick={togglePasswordVisibility}>
+            {passwordVisibility ? 'Hide' : 'Show'}
+          </Button>
+        </div>
+        <br />
+        {/* CreateAccountModal */}
+        <p>
+          Don't have an account? &nbsp;
+          <button id="register-button" onClick={handleOpenCreateAccountModal}>
+            Sign Up
+          </button>
+        </p>
 
-            <Modal.Footer>
-                <div className="container d-flex justify-content-between">
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button className='save-button' variant="primary" onClick={handleLogin}>
-                        Login
-                    </Button>
-                </div>
-            </Modal.Footer>
-        </Modal>
-    );
+        <CreateAccountModal
+          show={createAccountModalVisible}
+          handleClose={handleCloseCreateAccountModal}
+        />
+        <br />
+        <br />
+      </Modal.Body>
+
+      <Modal.Footer>
+        <div>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          &nbsp;&nbsp;
+          <Button variant="primary" onClick={handleLoginAttempt}>
+            Login
+          </Button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default LoginModal;
