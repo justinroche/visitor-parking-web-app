0a3491c5cedd8d8b183a9bb9675c7fa845e20afc
Cynthia <158005567+PhillipsUWW@users.noreply.github.com>
2024-04-22 22:18:01 -0500

    Update LoginModal.jsx
    design changes

--- client/src/LoginModal.jsx
+++ client/src/LoginModal.jsx
@@ -4,98 +4,146 @@ import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import './LoginModal.css';
 import CreateAccountModal from './CreateAccountModal';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-function LoginModal({ show, handleClose, isLoggedIn }) {
-    /* State */
-    const [createAccountModalVisible, setCreateAccountModalVisible] = useState(false);
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
-    const [passwordVisibility, setPasswordVisibility] = useState(false);
+import {faLock} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-    /* Handlers */
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({ ...formData, [name]: value });
-    };
+function LoginModal({ show, handleClose, handleLogin, fetchPasses }) {
+  /* State */
+  const [createAccountModalVisible, setCreateAccountModalVisible] =
+    useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
 
-    const togglePasswordVisibility = () => {
-        setPasswordVisibility(!passwordVisibility);
-    };
+  /* Handlers */
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleOpenCreateAccountModal = () => {
-        setCreateAccountModalVisible(true);
-    };
+  const togglePasswordVisibility = () => {
+    setPasswordVisibility(!passwordVisibility);
+  };
 
-    const handleCloseCreateAccountModal = () => {
-        setCreateAccountModalVisible(false);
-    };
+  const handleOpenCreateAccountModal = () => {
+    setCreateAccountModalVisible(true);
+  };
 
-    const handleLogin = () => {
-        // Perform login logic here
-        console.log('Logging in with:', formData);
-        handleClose();
-    };
+  const handleCloseCreateAccountModal = () => {
+    setCreateAccountModalVisible(false);
+  };
 
-    return (
-        <Modal show={show} onHide={handleClose} backdrop="static">
-            <Modal.Header closeButton>
-                <Modal.Title>Login</Modal.Title>
-            </Modal.Header>
+  const handleLoginAttempt = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/login-user',
+        formData
+      );
+      if (response.status === 200) {
+        await fetchPasses(formData.email); // Fetch user passes on successful login
+        handleClose(); // Close the modal on successful login
+        handleLogin(formData.email); // Update the login state
+      }
+    } catch (error) {
+      if (error.response) {
+        const status = error.response.status;
+        if (status === 401) {
+          alert('Invalid email or password.');
+        } else {
+          alert(`Login failed with error code: ${status}`);
+        }
+      } else {
+        alert('An error occurred during login. Please try again later.');
+      }
+    }
+  };
 
-            <Modal.Body>
-                {/* Email input */}
-                <form>
-                    <input
-                        type='email'
-                        id='emailInput'
-                        name='email'
-                        placeholder='Email Address'
-                        value={formData.email}
-                        onChange={handleInputChange}
-                    />
-                </form>
-                <br />
+  return (
+    <Modal show={show} onHide={handleClose} backdrop="static">
+      <Modal.Header closeButton>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
 
-                {/* Password input */}
-                <div>
-                    <input
-                        type={passwordVisibility ? 'text' : 'password'}
-                        id='passwordInput'
-                        name='password'
-                        placeholder='Password'
-                        value={formData.password}
-                        onChange={handleInputChange}
-                    />
-                    &nbsp;&nbsp;
-                    <Button className='save-button' id='password-button' onClick={togglePasswordVisibility}>
-                        {passwordVisibility ? 'Hide Password' : 'Show Password'}
-                    </Button>
-                </div>
-                <br />
+      <Modal.Body>
+        {/* Email input */}
+        <form>   
+            <div className='input-group'>
+            <span className="input-group-text">
+            <FontAwesomeIcon icon={faEnvelope} />
+                </span>        
+         <input
+                    
+            type="email"
+            id="emailInput"
+            name="email"
+            placeholder="Email Address"
+            value={formData.email}
+            onChange={handleInputChange}
+            tabIndex={1}
+            required
+          />
+          </div>
+        </form>
+        <br />
 
-                {/* CreateAccountModal */}
-                <p id='footer-text'>Don't have an account?&nbsp;&nbsp;<button type='button' onClick={handleOpenCreateAccountModal}>Register</button></p>
-                <CreateAccountModal
-                    show={createAccountModalVisible}
-                    handleClose={handleCloseCreateAccountModal}
-                />
-                <br /><br />
-            </Modal.Body>
+        {/* Password input */}
+        <div className='input-group'>
+          <span className="input-group-text">
+            <FontAwesomeIcon icon={faLock} />
+          </span>   
+        
+          <input
+            type={passwordVisibility ? 'text' : 'password'}
+            id="passwordInput"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
+            tabIndex={2}
+            required
+            
+          />
+          &nbsp;&nbsp;
+          <Button id="password-button" onClick={togglePasswordVisibility}>
+            <FontAwesomeIcon icon={passwordVisibility ? faEyeSlash : faEye} />
+          </Button>
+        </div>
+        
+        <br />
+        {/* CreateAccountModal */}
+        <p>
+          Don't have an account? &nbsp;
+          <button id="register-button" onClick={handleOpenCreateAccountModal}>
+            Sign Up
+          </button>
+        </p>
 
-            <Modal.Footer>
-                <div className="container d-flex justify-content-between">
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button className='save-button' variant="primary" onClick={handleLogin}>
-                        Login
-                    </Button>
-                </div>
-            </Modal.Footer>
-        </Modal>
-    );
+        <CreateAccountModal
+          show={createAccountModalVisible}
+          handleClose={handleCloseCreateAccountModal}
+        />
+        <br />
+        <br />
+      </Modal.Body>
+
+      <Modal.Footer>
+        <div>
+          <Button id='closebutton' variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          &nbsp;&nbsp;
+          <Button id = 'loginbutton' variant="primary" onClick={handleLoginAttempt}>
+            Login
+          </Button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default LoginModal;
