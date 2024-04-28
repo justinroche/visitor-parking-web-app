303fffc6ae00deee32057f9b59754a1a9c41d117
patyj6 <WhitneyPJ14@uww.edu>
2024-04-28 16:51:32 -0500

    TimeAddedModal

--- client/src/TimeAddedModal.jsx
+++ client/src/TimeAddedModal.jsx
@@ -5,17 +5,25 @@ import ModalBody from 'react-bootstrap/esm/ModalBody';
 import './TimeAddedModal.css';
 
 function TimeAddedModal({ show, handleClose }) {
+
+  function handleCloseBtn() {
+    handleClose();
+  }
+
+  function handleConfirmBtn() {
+    
+    handleClose();
+  }
+
   return (
     // modal front end
     <Modal show={show} onHide={handleClose} backdrop="static">
       <Modal.Header>
         <Modal.Title>An active pass for this license plate already exists. Do you want to add more time?</Modal.Title>
       </Modal.Header>
-      {/* <Button class="confirm">Confirm</Button>
-      <Button class="cancel">Close</Button> */}
       <ModalBody>
-        <button class='cancel'>Cancel</button>
-        <button class='confirm'>Confirm</button>
+        <button class="cancel" onClick={handleCloseBtn}>Cancel</button>
+        <button class="confirm" onClick={handleConfirmBtn}>Confirm</button>
       </ModalBody>
     </Modal>
   );
